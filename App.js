f8182559e9ebf32e4b64e67922639f5430742e43
Joehanz_senna <jonathanedison34@gmail.com>
2023-02-12 14:04:50 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Desktop from './components/Desktop'
+import './components/Desktop.css'
+
+function App() {
+  return (
+    <div>
+      <Desktop/>
+    </div>
+  );
+}
+
+export default App;
